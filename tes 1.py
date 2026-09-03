0d26296e5ecbf75e11fc1f34d7dd6f2d7a2cece3
ardelia surya agustin <ardeliasuryaa@gmail.com>
2026-09-03 19:20:02 +0700

    Add files via upload

---
+++ tes 1.py
@@ -0,0 +1,13 @@
+# Program Menghitung Luas Segitiga
+
+print("=== PROGRAM MENGHITUNG LUAS SEGITIGA ===")
+
+# Input
+alas = float(input("Masukkan alas segitiga (cm): "))
+tinggi = float(input("Masukkan tinggi segitiga (cm): "))
+
+# Proses
+luas = 0.5 * alas * tinggi
+
+# Output
+print("Luas segitiga adalah:", luas, "cm²")
